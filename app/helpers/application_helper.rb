74bc79002865bf9f32e34959e0bac97505f1bf9d
Andrea Wu <andreaowu@berkeley.edu>
2012-12-25 21:14:36 -0800

    first commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,6 @@
+module ApplicationHelper
+  def full_title(page_title)
+    full_title = "Ruby on Rails Tutorial App"
+    full_title += " | #{page_title}" unless page_title.blank?
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'af133e8c69f5060b392d6b7b8ec99d273d4830739565c3460093228cc88e522a3cf8560b35c85d574b009e9b5d143595074140ee99043569faba7eb4062248f6'
